300aad71b085c17412553bd7023756d41906d93d
Vann <111390478+Van-x2@users.noreply.github.com>
2024-10-24 09:24:37 +0000

    Created the search system and a crude way to display searches

--- web/src/routes/api/searchMongo/+server.ts
+++ web/src/routes/api/searchMongo/+server.ts
@@ -4,7 +4,14 @@ export const GET: RequestHandler = async ({ url }) => {
     // Extract data from search params
     const tag = url.searchParams.get('tag') || ''
     const searchString = url.searchParams.get('searchString') || ''
-
     console.log(`Tag:${tag} | Search:${searchString}`)
+
+    
+
+
+
+
+
+
     return json({ success: 'searching', tag, searchString })
 }

8b2ad1a114ebf025f9f262f4d064052e7c4bdc33
Vann <111390478+Van-x2@users.noreply.github.com>
2024-10-25 19:14:01 +0000

    Fixed mongodb connection issues on server startup

--- web/src/lib/mongoconnect.ts
+++ web/src/lib/mongoconnect.ts
@@ -4,18 +4,25 @@ import { MONGO_STRING } from "$env/static/private"
 let client: any | null = null
 
 export async function connectToDatabase() {
-    if(client && client.topology?.isConnected()) {
+    if (client && client.topology?.isConnected()) {
         console.log('connected to MongoDB on existing connection')
         return client
     }
-
     console.log('starting new connection to mongo')
-    client = new MongoClient(MONGO_STRING)
-    await client.connect()
-    return client
+    try {
+        client = new MongoClient(MONGO_STRING)
+        await client.connect()
+        // Test the connection
+        await client.db("admin").command({ ping: 1 })
+        console.log("Connected successfully to MongoDB")
+        return client
+    } catch (error) {
+        console.error("Connection error:", error)
+        throw error
+    }
 }
 
-export function getClient() {
+export async function getClient() {
     if (!client) {
         throw new Error('Call connectToDatabasefirst')
     }
